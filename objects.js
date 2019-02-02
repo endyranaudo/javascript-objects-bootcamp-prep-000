2976a9de8d2d741619305faf35290df0016720a4
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-02 15:50:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,13 @@
-var playlist = {
-  Nirvana : ”Lytium”
+var playlist = {} 
+playlist.Beyonce = 'fear' 
+playlist['BTS'] = 'fire' 
+
+ 
+
+function updatePlaylist(playlist, artist, song) { 
+  playlist[artist]= song 
+  return playlist
 }
+
+updatePlaylist(playlist,'Taylor', 'red')
+console.log(playlist)
