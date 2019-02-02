a787d1637ff18ebb38c63a06d21f1f5e5dc718b7
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-02 15:38:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,3 @@ let playlist = {
   Phil Ochs: "Here's to the State of Mississippi"
 }
 
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist.Slowdive = "Allison";
-  playlist["My Bloody Valentine"] = "Sometimes";
-}
