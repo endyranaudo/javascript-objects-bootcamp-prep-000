be1abbd23c5d07ec720d3737f0f4c3fded550feb
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-02 15:46:46 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,10 @@
-let playlist = {
-  PhilOchs: "Here's to the State of Mississippi"
+
+
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
 }
